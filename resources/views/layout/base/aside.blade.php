bc017c7a98a5b11fb6ae8e52fd298ff6e30b11d9
andiabdillahdev <andiabdillahdev@gmail.com>
2023-04-01 15:03:02 +0800

    aside

--- resources/views/layout/base/aside.blade.php
+++ resources/views/layout/base/aside.blade.php
@@ -378,28 +378,30 @@ $path = explode('/', Request::path());
 
                      @endif
 
-                     <li class="menu-item {{ $path[0] == 'admin' && $path[1] == 'absen' ? 'menu-item-active' : '' }}"
-                        aria-haspopup="true">
-                        <a href="{{ route('absen') }}" class="menu-link">
-                            <span class="svg-icon menu-icon">
+                     @if(Session::get('user.role') == 'pegawai' || Session::get('user.role') == 'super_admin')
+                        <li class="menu-item {{ $path[0] == 'admin' && $path[1] == 'absen' ? 'menu-item-active' : '' }}"
+                            aria-haspopup="true">
+                            <a href="{{ route('absen') }}" class="menu-link">
+                                <span class="svg-icon menu-icon">
 
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <polygon points="0 0 24 0 24 24 0 24" />
-                                        <path
-                                            d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z"
-                                            fill="#000000" fill-rule="nonzero" opacity="0.3" />
-                                        <path
-                                            d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z"
-                                            fill="#000000" fill-rule="nonzero" />
-                                    </g>
-                                </svg>
-                                
-                            </span>
-                            <span class="menu-text text-capitalize">Kehadiran</span>
-                        </a>
-                    </li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <polygon points="0 0 24 0 24 24 0 24" />
+                                            <path
+                                                d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z"
+                                                fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                                            <path
+                                                d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z"
+                                                fill="#000000" fill-rule="nonzero" />
+                                        </g>
+                                    </svg>
+                                    
+                                </span>
+                                <span class="menu-text text-capitalize">Kehadiran</span>
+                            </a>
+                        </li>
+                     @endif
                      
                     <li class="menu-item menu-item-submenu {{ $path[0] == 'laporan-admin' ? 'menu-item-open' : '' }}"
                         aria-haspopup="true" data-menu-toggle="hover">
